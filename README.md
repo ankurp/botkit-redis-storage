8d1dfae249ce472ed2bb1133ecf414ca30b845d7
Ankur Patel <apatel@shutterstock.com>
2019-04-13 06:56:50 -0400

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Botkit Redis Storage
+
+Redis Store for Botkit bots. This is similar to the JSON Storage but instead of storing in a file system as JSON files it stores data in Redis.
---
+++ index.js
@@ -0,0 +1,46 @@
+const { promisify } = require('util');
+const redis = require('redis');
+
+module.exports = function({ port, hostname, options } = {}) {
+  const client = redis.createClient(port, hostname, options);
+  for (let key in client) {
+    if (typeof client[key] === 'function') {
+      client[`${key}Async`] = promisify(client[key]).bind(client);
+    }
+  }
+  
+  const store = (type) => ({
+    get: async function(id, cb = () => {}) {
+      const key = id.startsWith(type) ? id : `${type}-${id}`;
+      const result = await client.hgetallAsync(key);
+      cb(result);
+      return result;
+    },
+    save: async function(data, cb = () => {}) {
+      for (let key in data) {
+        await client.hsetAsync(`${type}-${data.id}`, key, data[key]);
+      }
+      cb();
+      return data;
+    },
+    delete: async function(id, cb = () => {}) {
+      await client.hdelAsync(`${type}-${id}`);
+      cb();
+      return id;
+    },
+    all: async function(cb = () => {}) {
+      const keys = await client.keysAsync(`${type}-*`);
+      const result = await Promise.all(keys.map(k => this.get(k)));
+      cb(result);
+      return result;
+    }
+  });
+
+  var storage = {
+    teams: store('teams'),
+    users: store('users'),
+    channels: store('channels')
+  };
+
+  return storage;
+};
